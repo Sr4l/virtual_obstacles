56fe8ef2dcb53482bc9e53082b6ee8ae56776084
Sr4l <LarsKistner@gmail.com>
2015-05-21 14:42:32 +0200

    handle route in moving_obstalce_msg

--- include/virtual_obstacles/moving_objects.h
+++ include/virtual_obstacles/moving_objects.h
@@ -4,6 +4,7 @@
 // std lib includes
 #include <iostream>
 #include <string>
+#include <vector>
 #include <map>
 
 // ros includes
@@ -26,20 +27,20 @@ public:
   MovingObjects();
 
   virtual void onInitialize();
-  
+
   virtual void updateBounds(double origin_x, double origin_y, double origin_yaw,
                   double* min_x, double* min_y, double* max_x, double* max_y);
-  
+
   virtual void updateCosts(costmap_2d::Costmap2D& master_grid, int min_i,
                   int min_j, int max_i, int max_j);
-  
+
   virtual void matchSize();
-  
+
   bool isDiscretized()
   {
     return true;
   }
-  
+
 private:
   /*
    * Typdefs and structs
@@ -54,50 +55,58 @@ private:
     ros::Time last_msg;
     Coord2D position;
     Coord2D velocity;
+    std::vector<geometry_msgs::PoseStamped> route;
+    //std::list<Coord2D> route;
   };
 
   typedef std::map<std::string, State> Dict;
   typedef Dict::const_iterator DictIt;
-  
+  typedef std::vector<geometry_msgs::PoseStamped>::iterator RouteIt;
+
   struct UpdateBoundingBox {
     double min_x;
     double max_x;
     double min_y;
     double max_y;
   };
-  
+
   /*
    * Privat methods
    */
   void reconfigureCB(virtual_obstacles::GenericPluginConfig &config, uint32_t level);
-  
+
   void drawCircle(double circle_radius, double center_x, double center_y);
-  
+
   void drawSimpleRectangle(double start_x, double start_y, double end_x, double end_y);
-  
+
   void drawPoint(double x, double y);
-  
+
   void msgMovingObjectCB(const virtual_obstacles::moving_object_msg::ConstPtr& msg);
-  
+
   void deleteOldDataSets();
-  
+
+  void placeObstalcesInMap(double origin_x, double origin_y, double origin_yaw);
+
   /*
    * class wide variables
    */
-   
+
   // dyn reconfigure
   dynamic_reconfigure::Server<virtual_obstacles::GenericPluginConfig> *dsrv_;
   double_t update_zone_enlargment;
   std::string own_unique_name;
   double_t obstacle_life_time;
-  
+
+  double_t max_obstical_scalling_distance;
+  double_t min_obstical_scalling_distance;
+
   // msg system
   ros::NodeHandle nh;
   ros::Subscriber sub;
-  
+
   // moving objects store
   Dict moving_objects_store;
-  
+
   // store min/max x and y to update
   UpdateBoundingBox update_area;
 
--- src/moving_objects.cpp
+++ src/moving_objects.cpp
@@ -16,15 +16,19 @@ namespace virtual_obstacles
  * initalisation and config handling
  * #####################################################################
  */
- 
+
 /* *********************************************************************
  * constructor
  */
 MovingObjects::MovingObjects()
 {
-  update_zone_enlargment = 0.5;
+  update_zone_enlargment = 1.0;
   own_unique_name = "";
   obstacle_life_time = 3.0;
+
+  // ToDo: make this dyn reconfigure option
+  max_obstical_scalling_distance = 2.0;
+  min_obstical_scalling_distance = 0.5;
 }
 
 /* *********************************************************************
@@ -36,7 +40,7 @@ void MovingObjects::onInitialize()
 
   // register msg handling
   sub = nh.subscribe("/robot_state_publisher", 100, &MovingObjects::msgMovingObjectCB, this);
-  
+
   // set plugin vars
   current_ = true;
   default_value_ = NO_INFORMATION;
@@ -51,7 +55,7 @@ void MovingObjects::onInitialize()
 
 /* *********************************************************************
  * matchSize
- * 
+ *
  * match size between master grid and local grid
  */
 void MovingObjects::matchSize()
@@ -63,7 +67,7 @@ void MovingObjects::matchSize()
 
 /* *********************************************************************
  * reconfigureCB
- * 
+ *
  * dynamic reconfigure callback
  */
 void MovingObjects::reconfigureCB(virtual_obstacles::GenericPluginConfig &config, uint32_t level)
@@ -80,9 +84,80 @@ void MovingObjects::reconfigureCB(virtual_obstacles::GenericPluginConfig &config
  */
 
 /* *********************************************************************
+ * placeObstalcesInMap
+ *
+ * place obstalces in map
+ */
+void MovingObjects::placeObstalcesInMap(double origin_x, double origin_y,
+      double origin_yaw)
+{
+  // draw obstacles from moving obstacles store
+  double x = 0.0;
+  double y = 0.0;
+  double yaw = 0.0;
+
+  double vx = 0.0;
+  double vy = 0.0;
+  double vres = 0.0;
+
+  double distance_to_origin = 0.0;
+  double scale_factor = 0.0;
+
+  for (DictIt it(moving_objects_store.begin()); it != moving_objects_store.end(); it++)
+  {
+    // if obstacles unique_name same as own unique_name, ignore it
+    if (it->first == own_unique_name)
+      continue;
+
+    // short names for position
+    x = moving_objects_store[it->first].position.x;
+    y = moving_objects_store[it->first].position.y;
+    yaw = moving_objects_store[it->first].position.theta;
+
+    // calculate distance to origin in meter for dynamic scaling
+    distance_to_origin = std::sqrt( std::pow((x-origin_x), 2) + std::pow((y-origin_y), 2) );
+    scale_factor = ((distance_to_origin-min_obstical_scalling_distance) / (max_obstical_scalling_distance-min_obstical_scalling_distance));
+    scale_factor = std::min(1.0, scale_factor);
+    scale_factor = 1.0 - scale_factor;
+
+    // short names for velocity
+    vx = moving_objects_store[it->first].velocity.x;
+    vy = moving_objects_store[it->first].velocity.y;
+    vres = std::sqrt( std::pow(vx, 2) + std::pow(vy, 2) );
+
+    // block moving obstacle center point without exception
+    drawPoint(x, y);
+
+    // block moving obstacle footprint, scaled by distance to robot
+    // low distance -> full size
+    // high distance -> small (to no) size
+    // ToDo: save obstacle shape in Msg
+    drawCircle(0.18*scale_factor, x, y);
+
+    // block moving obstacle route (smart or stupid way)
+    if (moving_objects_store[it->first].route.size() > 0)
+    {
+      // block moving obstacle planed route
+      for (RouteIt it2= moving_objects_store[it->first].route.begin();
+        it2 != moving_objects_store[it->first].route.end(); it2++)
+      {
+        drawPoint(it2->pose.position.x, it2->pose.position.y);
+      }
+    } else {
+      //block moving obstacle way, based on velocity (stupid way)
+      for (int i = 0; i < 20; i++)
+      {
+        drawPoint(x + cos(yaw)*vres*i/10.0, y + sin(yaw)*vres*i/10.0);
+      }
+    }
+  }
+
+}
+
+/* *********************************************************************
  * updateBounds
- * 
- * update obstacles and bounds 
+ *
+ * update obstacles and bounds
  */
 void MovingObjects::updateBounds(double origin_x, double origin_y,
       double origin_yaw, double* min_x, double* min_y, double* max_x,
@@ -90,56 +165,26 @@ void MovingObjects::updateBounds(double origin_x, double origin_y,
 {
   if (!enabled_)
     return;
-  
+
   // ros spine once to process new messages
   ros::spinOnce();
-  
+
   // cleanup old datasets in moving_objects_store
   deleteOldDataSets();
-  
+
   // reset map
   for (int ind = 0; ind < (size_y_*size_x_); ind++)
     costmap_[ind] = NO_INFORMATION;
-    
+
   // reset update bound
   update_area.min_x  = *min_x;
   update_area.max_x  = *max_x;
   update_area.min_y  = *min_y;
   update_area.max_y  = *max_y;
-  
-  // draw obstacles from moving obstacles store
-  double x = 0.0;
-  double y = 0.0;
-  double yaw = 0.0;
-  
-  double vx = 0.0;
-  double vy = 0.0;
-  double vres = 0.0;
-  
-  for (DictIt it(moving_objects_store.begin()); it != moving_objects_store.end(); it++)
-  {
-    std::cout << "Element: " << it->first << " = " << moving_objects_store[it->first].last_msg << std::endl;
-    
-    // if obstacles unique_name same as own unique_name, ignore it
-    if (it->first == own_unique_name)
-      continue;
-    
-    x = moving_objects_store[it->first].position.x;
-    y = moving_objects_store[it->first].position.y;
-    yaw = moving_objects_store[it->first].position.theta;
-    
-    vx = moving_objects_store[it->first].velocity.x;
-    vy = moving_objects_store[it->first].velocity.y;
-    vres = std::sqrt( std::pow(vx, 2) + std::pow(vy, 2) );
-    
-    
-    drawCircle(0.18, x, y);
-    drawPoint(x + cos(yaw)*vres*1, y + sin(yaw)*vres*1);
-    drawPoint(x + cos(yaw)*vres*2, y + sin(yaw)*vres*2);
-    drawPoint(x + cos(yaw)*vres*3, y + sin(yaw)*vres*3);
-    
-  }
-  
+
+  // place obstacles
+  placeObstalcesInMap(origin_x, origin_y, origin_yaw);
+
   // update bounds
   *min_x = std::min(*min_x, update_area.min_x - update_zone_enlargment);
   *min_y = std::min(*min_y, update_area.min_y - update_zone_enlargment);
@@ -149,7 +194,7 @@ void MovingObjects::updateBounds(double origin_x, double origin_y,
 
 /* *********************************************************************
  * updateCosts
- * 
+ *
  * updates the master grid in the area defined in updateBounds
  */
 void MovingObjects::updateCosts(costmap_2d::Costmap2D& master_grid,
@@ -157,7 +202,7 @@ void MovingObjects::updateCosts(costmap_2d::Costmap2D& master_grid,
 {
   if (!enabled_)
     return;
-  
+
   for (int j = min_j; j < max_j; j++)
   {
     for (int i = min_i; i < max_i; i++)
@@ -165,7 +210,7 @@ void MovingObjects::updateCosts(costmap_2d::Costmap2D& master_grid,
       int index = getIndex(i, j);
       if (costmap_[index] == NO_INFORMATION)
         continue;
-      master_grid.setCost(i, j, costmap_[index]); 
+      master_grid.setCost(i, j, costmap_[index]);
     }
   }
 }
@@ -177,25 +222,21 @@ void MovingObjects::updateCosts(costmap_2d::Costmap2D& master_grid,
 
 void MovingObjects::msgMovingObjectCB(const virtual_obstacles::moving_object_msg::ConstPtr& msg)
 {
-  // Debug
-  //ROS_INFO("I heard from  [%s]:", msg->unique_name.c_str());
-  //std::cout << "Position: " << msg->pose.x << " / " << msg->pose.y << " / " << msg->pose.theta << std::endl;
-  //std::cout << "Velocity: " << msg->velocity.linear.x << " / " << msg->velocity.linear.y << " / " << msg->velocity.angular.z << std::endl;
-  
   // Save Msg data in Object Store
   State s;
   s.last_msg = ros::Time::now();
-  
+
   s.position.x = msg->pose.x;
   s.position.y = msg->pose.y;
   s.position.theta = msg->pose.theta;
-  
+
   s.velocity.x = msg->velocity.linear.x;
   s.velocity.y = msg->velocity.linear.y;
   s.velocity.theta = msg->velocity.angular.z;
-  
+
+  s.route = msg->route;
+
   moving_objects_store[msg->unique_name.c_str()] = s;
-  
 }
 
 /* #####################################################################
@@ -205,36 +246,36 @@ void MovingObjects::msgMovingObjectCB(const virtual_obstacles::moving_object_msg
 
 /* *********************************************************************
  * drawCircle
- * 
+ *
  * draw a circle on local grid (costmap_)
  */
 void MovingObjects::drawCircle(double circle_radius, double center_x, double center_y)
 {
   double tol = 0.01;
-  
+
   unsigned int pixle_center_x;
   unsigned int pixle_center_y;
-  
+
   unsigned int pixle_tmp_x;
   unsigned int pixle_tmp_y;
-  
+
   worldToMap(center_x, center_y, pixle_center_x, pixle_center_y);
   worldToMap(center_x + circle_radius, center_y, pixle_tmp_x, pixle_tmp_y);
-  
+
   int max_distance = pixle_tmp_x - pixle_center_x;
-  
-  
+
+
   for (int cx = -max_distance; cx <= max_distance; cx++)
   {
     for (int cy = -max_distance; cy <= max_distance; cy++)
     {
       float distance_to_centre = sqrt(pow(((double)cx/(double)max_distance), 2) + pow(((double)cy/(double)max_distance), 2));
-      
+
       if (distance_to_centre < 1.0)
         setCost(pixle_center_x + cx, pixle_center_y + cy, LETHAL_OBSTACLE);
     }
   }
-  
+
   // update "update area"
   update_area.min_x = std::min( update_area.min_x, center_x - circle_radius);
   update_area.max_x = std::max( update_area.max_x, center_x + circle_radius);
@@ -244,7 +285,7 @@ void MovingObjects::drawCircle(double circle_radius, double center_x, double cen
 
 /* *********************************************************************
  * drawSimpleRectangle
- * 
+ *
  * draw a simple rectangle on local grid (costmap_), simple means no
  * rotation
  */
@@ -255,10 +296,10 @@ void MovingObjects::drawSimpleRectangle(double start_x, double start_y, double e
   unsigned int pixle_start_y;
   unsigned int pixle_end_x;
   unsigned int pixle_end_y;
-  
+
   worldToMap(start_x, start_y, pixle_start_x, pixle_start_y);
   worldToMap(end_x, end_y, pixle_end_x, pixle_end_y);
-  
+
   for (int i = pixle_start_x; i < pixle_end_x; i++)
   {
     setCost(i, pixle_start_y, LETHAL_OBSTACLE);
@@ -270,7 +311,7 @@ void MovingObjects::drawSimpleRectangle(double start_x, double start_y, double e
     setCost(pixle_start_x, i, LETHAL_OBSTACLE);
     setCost(pixle_end_x, i, LETHAL_OBSTACLE);
   }
-  
+
   // update "update area"
   update_area.min_x = std::min( update_area.min_x, std::min(start_x, end_x));
   update_area.max_x = std::max( update_area.max_x, std::max(start_x, end_x));
@@ -280,14 +321,14 @@ void MovingObjects::drawSimpleRectangle(double start_x, double start_y, double e
 
 /* *********************************************************************
  * drawPoint
- * 
+ *
  * draw a Point on local grid (costmap_)
  */
 void MovingObjects::drawPoint(double x, double y)
 {
   unsigned int pixle_x;
   unsigned int pixle_y;
-  
+
   worldToMap(x, y, pixle_x, pixle_y);
 
   setCost(pixle_x, pixle_y, LETHAL_OBSTACLE);
@@ -306,7 +347,7 @@ void MovingObjects::drawPoint(double x, double y)
 
 /* *********************************************************************
  * deleteOldDataSets
- * 
+ *
  * delete old datasets from moving_objects_store
  */
 void MovingObjects::deleteOldDataSets()
